0dc659e8f68b31357192833d810518c12cc6ee94
Gabriel Takahashi <gabrltakahashi@gmail.com>
2023-06-12 08:54:13 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../CSS/Header.css';
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <h5>Teste Tecnico</h5>
+      <h1>Shopper</h1>
+      <a href="https://github.com/thidurante">Thiago Durante</a>
+    </div>
+  );
+}
