a6bec79bf889d68fb8301b4a08bb2bb702c6ea37
Gabriel Takahashi <gabrltakahashi@gmail.com>
2023-06-14 09:30:49 -0300

    Add router and chakraUi to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home/index'
+import Maps from './components/maps/index'
+import Table from './components/table/index'
+import Header from './components/header/index'
 
+import { ChakraProvider } from '@chakra-ui/react'
 function App() {
   return (
-    <div className="main-container">
-      <p> home page </p>
-    </div>
+    <Router>
+      <ChakraProvider>
+        <Header/>
+        <Routes>
+            <Route path='/' Component={Home} />
+            <Route path='/maps' Component={Maps} />
+            <Route path='/table' Component={Table} />
+        </Routes>
+      </ChakraProvider>
+    </Router>
   );
 }
 
---
+++ src/components/maps/index.tsx
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class Maps extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Maps</h2>
+      </div>
+    );
+  }
+}
+
+export default Maps;
