957b8fb6f48c2b169b40a345370d52bcf9dd8f7a
Gabriel <gabriel.takahashi2020@gmail.com>
2023-07-20 08:40:52 -0300

    adiciona novo modal de exclusão

--- src/context/modal/index.tsx
+++ src/context/modal/index.tsx
@@ -1,5 +1,7 @@
 import { EditDataModalProvider } from './edit-data-modal'
+import { ConfirmDeleteModalProvider } from './confirm-delete-modal'
 export * from './edit-data-modal'
+export * from './confirm-delete-modal'
 
 interface ModalProviderProps {
     children: React.ReactNode
@@ -7,7 +9,9 @@ interface ModalProviderProps {
 export const ModalProvider = ({children}: ModalProviderProps) => {
     return (
         <EditDataModalProvider>
-            {children}
+            <ConfirmDeleteModalProvider>
+                {children}
+            </ConfirmDeleteModalProvider>
         </EditDataModalProvider>
     )
 }
